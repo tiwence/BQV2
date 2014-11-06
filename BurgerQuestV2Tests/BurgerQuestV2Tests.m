5733ffc79f1d4810d515ab7c6749142358cffa65
tiwence <terence.marill@gmail.com>
2014-11-06 14:56:48 +0100

    Adding ipa and xcode project

---
+++ BurgerQuestV2Tests/BurgerQuestV2Tests.m
@@ -0,0 +1,34 @@
+//
+//  BurgerQuestV2Tests.m
+//  BurgerQuestV2Tests
+//
+//  Created by Térence Marill on 24/02/2014.
+//  Copyright (c) 2014 BurgerQuest. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BurgerQuestV2Tests : XCTestCase
+
+@end
+
+@implementation BurgerQuestV2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
